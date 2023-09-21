a7877ee41d0a3004eddc1a6d24667d7163e1d837
dakixr <dakixr@gmail.com>
2023-09-21 13:55:31 +0200

    update args

--- src/isbtchot/__main__.py
+++ src/isbtchot/__main__.py
@@ -6,16 +6,16 @@ parser = argparse.ArgumentParser(description="BTC Hotness Index", prog="isbtchot
 
 # Add an optional positional argument named 'year' with default value of 6
 parser.add_argument(
-    "--periods_back",
     "-p",
+    "--periods_back",
     type=int,
     default=85,
     help="Periods back to be processed. Defaults to 50 if not provided.",
 )
 
 parser.add_argument(
-    "--time",
     "-t",
+    "--time",
     type=TypeTime,
     default=TypeTime.WEEK,
     help="Candle stick time to use. Defaults to W (Weekly),"
